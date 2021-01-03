0710e8439c612c37cff5dd09d6ee9a5b1fd2ea95
harishtallapalli <76802666+harishtallapalli@users.noreply.github.com>
2021-01-03 12:04:39 +0530

    Update kiran.tf

--- kiran.tf
+++ kiran.tf
@@ -3,16 +3,11 @@ provider "aws" {
         access_key = "AKIA2SUNEGF5GCAR3W5U"
         secret_key = "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"
 }
-resource "aws_instance" "kalyan" {
-        ami = "ami-0be2609ba883822ec"
-        instance_type = "t2.micro"
-        key_name = "Ajji_perm"
-        tags = {
-        Name = "kalyan"
-}
+
+
 }
 resource "aws_s3_bucket" "kalyan-test" {
-        bucket = "ajji19822"
+        bucket = "ajji19823"
 }
 
 
